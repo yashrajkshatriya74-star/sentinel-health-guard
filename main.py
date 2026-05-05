4567931cf077ec6f436eacab5b6d4f1a48998fde
yashrajkshatriya74-star <yashrajkshatriya74@gmail.com>
2026-05-05 19:27:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -377,9 +377,11 @@ if __name__ == "__main__":
     ])
 
     uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=port,
-        proxy_headers=True,
-        forwarded_allow_ips="*"
-    )
+    app,
+    host="0.0.0.0",
+    port=port,
+    proxy_headers=True,
+    forwarded_allow_ips="*",
+    server_header=False,
+    host_header=None
+)
